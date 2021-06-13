cbc5c3611b0e2f1fa421d1f209c4e9f952e32cc3
Vishnu_G <vishnug9539@gmail.com>
2021-06-13 20:27:45 +0530

    implement nav hiding

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 
-
+const navMenu=document.getElementById("nav_menu"),
+      navToggle=document.getElementById("nav_toggle"),
+      navClose=document.getElementById("nav_close");
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add("show-menu");
+        console.log("click detected");
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
