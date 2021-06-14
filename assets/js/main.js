62ca1addbdb077f6cf9c01e943773f0c650574d2
Vishnu_G <vishnug9539@gmail.com>
2021-06-14 14:22:13 +0530

    added some content and changed stylesheet

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,26 @@ const navMenu=document.getElementById("nav_menu"),
 if(navToggle){
     navToggle.addEventListener('click',()=>{
         navMenu.classList.add("show-menu");
-        console.log("click detected");
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener("click",()=>{
+        navMenu.classList.remove("show-menu");
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav_menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
