1368082b33ea80976ce3f69d7a4f562395cbac48
Vishnu_G <vishnug9539@gmail.com>
2021-06-16 15:22:06 +0530

    completed qualification section

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,28 @@ skillsHeader.forEach((el)=>{
 })
 /*==================== QUALIFICATION TABS ====================*/
 
+const tabs=document.querySelectorAll('[data-target]'),
+      tabContents=document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab=>{
+    tab.addEventListener("click",()=>{
+        console.log("da mwone")
+        
+        const target=document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent=>{
+            tabContent.classList.remove("qualification_active")
+        })
+
+        target.classList.add("qualification_active")
+
+        tabs.forEach(tab=>{
+            tab.classList.remove("qualification_active")
+        })
+
+        tab.classList.add("qualification_active")
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
